a3ddf009e52e61c8198b143d3bbb52e3d4586dbb
Fernando Salamanca <william.salamanca02@uptc.edu.co>
2025-03-25 11:28:57 -0500

    feat: inject mongo and postgres repository implementations, and mines, mine nodes 3d routes

--- cmd/mines_microservice/main.go
+++ cmd/mines_microservice/main.go
@@ -7,23 +7,54 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/wFercho/mines_microservice/internal/application/usecase"
 	"github.com/wFercho/mines_microservice/internal/config"
-	db "github.com/wFercho/mines_microservice/internal/db/postgres"
-	"github.com/wFercho/mines_microservice/internal/routes"
+	mongo_db "github.com/wFercho/mines_microservice/internal/db/mongo"
+	postgres_db "github.com/wFercho/mines_microservice/internal/db/postgres"
+	"github.com/wFercho/mines_microservice/internal/server/handlers"
+	"github.com/wFercho/mines_microservice/internal/server/routes"
+
+	"github.com/rs/cors"
 )
 
 func main() {
-	db.ConnectToPostgresDatabase()
+	psconn, _ := postgres_db.ConnectToPostgresDatabase()
+	mongodbconn := mongo_db.ConnectToMongoDatabase()
 
 	if config.LoadConfig().Environment == "dev" {
-		db.SeedDB()
+		mine_id := postgres_db.SeedPostgresDB(psconn)
+		err := mongo_db.NewMineNodes3DSeeder(mongodbconn).SeedFromCSV("./test_data/mine_nodes3d.csv", mine_id)
+
+		if err != nil {
+			fmt.Println("error", err)
+		}
 	}
+
+	defer mongo_db.DisconnectMongo()
+
+	mineRepo := postgres_db.NewPostgresRepository(psconn)
+	mineUsecase := usecase.NewMineUseCase(mineRepo)
+	mineHandler := handlers.NewMineHandler(mineUsecase)
+
+	mineNodes3dRepo := mongo_db.NewMineNodes3DMongoRepository(mongodbconn)
+	mineNodes3dUseCase := usecase.NewMineNodes3DUseCase(mineNodes3dRepo)
+	mineNodes3dHandler := handlers.NewMineNodes3DHandler(mineNodes3dUseCase)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	// Agregar rutas
-	r.Mount("/api", routes.MineRoutes())
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // Permite todos los orígenes
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	r.Use(corsHandler.Handler)
+	routes.RegisterMinesRoutes(r, mineHandler)
+	routes.RegisterMineNodes3DRoutes(r, mineNodes3dHandler)
 
 	fmt.Println("Servidor escuchando en :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
